dbea77dd2b58dd83f4e5b3997864b0b8b291dce2
jshjonasss <108204677+jshjonasss@users.noreply.github.com>
2023-10-16 22:28:37 -0500

    Update q7.sql
    first name only

--- q7.sql
+++ q7.sql
@@ -1,5 +1,5 @@
-SELECT e1.first_name || ' ' || e1.last_name AS employee_name_1,
-       e2.first_name || ' ' || e2.last_name AS employee_name_2
+SELECT e1.first_name,
+       e2.first_name
 FROM employees e1
 JOIN employees e2 ON e1.department_id = e2.department_id
                   AND e1.manager_id = e2.manager_id
@@ -7,4 +7,4 @@ JOIN employees e2 ON e1.department_id = e2.department_id
 WHERE e1.employee_id <> e2.employee_id
       AND e1.salary > 10000 
       AND e2.salary > 10000
-ORDER BY e1.first_name, e1.last_name, e2.first_name, e2.last_name;
+ORDER BY e1.first_name, e2.first_name;
